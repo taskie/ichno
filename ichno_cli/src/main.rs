f15e0e1040a3653bedda4adc0e44f0c299d86585
taskie <t@skie.jp>
2020-07-25 00:28:50 +0900

    ichno-cli: use structopt

--- ichno_cli/src/main.rs
+++ ichno_cli/src/main.rs
@@ -8,8 +8,28 @@ use diesel::{connection::Connection, sqlite::SqliteConnection};
 use dotenv;
 use ignore;
 use twox_hash::RandomXxHashBuilder64;
+use structopt::{clap, StructOpt};
 
 use ichno::{db::SqliteStats, file, DEFAULT_NAMESPACE_ID};
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "ichno")]
+#[structopt(long_version(option_env!("LONG_VERSION").unwrap_or(env!("CARGO_PKG_VERSION"))))]
+#[structopt(setting(clap::AppSettings::ColoredHelp))]
+pub struct Opt {
+    #[structopt(subcommand)]
+    sub: SubCommands,
+}
+
+#[derive(Debug, StructOpt)]
+pub enum SubCommands {
+    Scan(Scan),
+}
+
+#[derive(Debug, StructOpt)]
+pub struct Scan {
+    // nop
+}
 
 fn main_with_error() -> Result<i32, Box<dyn Error>> {
     dotenv::dotenv().ok();
@@ -22,44 +42,48 @@ fn main_with_error() -> Result<i32, Box<dyn Error>> {
     ichno::db::migrate(&conn)?;
 
     let namespace_id = DEFAULT_NAMESPACE_ID;
-    let mut ctx = file::Context {
-        connection: &conn,
-        db_path: &db_path,
-        namespace_id,
-        namespace: None,
-        current_time: Local::now(),
-    };
-    let path = Path::new(".");
-
-    let w = {
-        let mut wb = ignore::WalkBuilder::new(&path);
-        wb.filter_entry(|p| p.file_name() != OsStr::new(".git") && p.file_name() != OsStr::new("ichno.db"));
-        wb.build()
-    };
-    file::pre_process(&mut ctx)?;
-    let mut path_set: HashSet<_, RandomXxHashBuilder64> = Default::default();
-    for result in w {
-        match result {
-            Ok(entry) => {
-                if entry.metadata().unwrap().is_file() {
-                    let path = file::upsert_with_file(&ctx, namespace_id, entry.path())?.path;
-                    path_set.insert(path);
+    let opt = Opt::from_args();
+    match opt.sub {
+        SubCommands::Scan(_) => {
+            let mut ctx = file::Context {
+                connection: &conn,
+                db_path: &db_path,
+                namespace_id,
+                namespace: None,
+                current_time: Local::now(),
+            };
+            let path = Path::new(".");
+            let w = {
+                let mut wb = ignore::WalkBuilder::new(&path);
+                wb.filter_entry(|p| p.file_name() != OsStr::new(".git") && p.file_name() != OsStr::new("ichno.db"));
+                wb.build()
+            };
+            file::pre_process(&mut ctx)?;
+            let mut path_set: HashSet<_, RandomXxHashBuilder64> = Default::default();
+            for result in w {
+                match result {
+                    Ok(entry) => {
+                        if entry.metadata().unwrap().is_file() {
+                            let path = file::upsert_with_file(&ctx, namespace_id, entry.path())?.path;
+                            path_set.insert(path);
+                        }
+                    }
+                    Err(err) => warn!("{}", err),
                 }
             }
-            Err(err) => warn!("{}", err),
+            let stats = SqliteStats::select(&conn, namespace_id)?;
+            for stat in stats.iter() {
+                if path_set.contains(&stat.path) {
+                    continue;
+                }
+                let path = Path::new(&stat.path);
+                if !path.exists() {
+                    file::remove_with_file(&ctx, namespace_id, path)?;
+                }
+            }
+            file::post_process(&mut ctx)?;
         }
-    }
-    let stats = SqliteStats::select(&conn, namespace_id)?;
-    for stat in stats.iter() {
-        if path_set.contains(&stat.path) {
-            continue;
-        }
-        let path = Path::new(&stat.path);
-        if !path.exists() {
-            file::remove_with_file(&ctx, namespace_id, path)?;
-        }
-    }
-    file::post_process(&mut ctx)?;
+    };
     Ok(0)
 }
 
