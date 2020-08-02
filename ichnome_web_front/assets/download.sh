46062e19963b86a8263b6002eab1f17cc6de3245
taskie <t@skie.jp>
2020-08-02 21:52:09 +0900

    ichnome_web_front: remove unneeded file

--- ichnome_web_front/assets/download.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -eu
-
-PROGRAM="$(realpath "$0")"
-PROGRAM_DIR="$(dirname "$PROGRAM")"
-
-cd "$PROGRAM_DIR"
-exec curl -LO https://raw.githubusercontent.com/highlightjs/highlight.js/master/src/styles/solarized-light.css
